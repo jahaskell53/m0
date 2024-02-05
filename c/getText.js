84bfb285babb9052d1befb6b671f36bfd8106877
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-02-05 18:25:42 +0000

    sort of working version, crawls web pages

--- c/getText.js
+++ c/getText.js
@@ -4,6 +4,7 @@
 
 const { convert } = require('html-to-text');
 const readline = require('readline');
+const natural = require('natural');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,10 +12,19 @@ const rl = readline.createInterface({
 
 // TODO Add some code
 
+let html = '';
+
 rl.on('line', (line) => {
   // TODO Add some code
+  // process.stdout.write(line);
+  html += line;
 });
 
 rl.on('close', () => {
   // TODO Add some code
+  const text = convert(html);
+  const tokenizer = new natural.WordTokenizer();
+  const tokens = tokenizer.tokenize(text);
+
+  process.stdout.write(tokens.join('\n'));
 });
--- c/getURLs.js
+++ c/getURLs.js
@@ -5,18 +5,30 @@ const { JSDOM } = require('jsdom');
 const { URL } = require('url');
 
 // Example usage:
-const baseURL = process.argv[2] + "/";
+// console.log(process.argv[0])
+const inputURL = process.argv[2];
+const baseURL = new URL(inputURL);
 
 const rl = readline.createInterface({
   input: process.stdin,
+  output: process.stdout
 });
 
 // TODO some code
-
+let urls = "";
 rl.on('line', (line) => {
   // TODO some code
+    const regex = /href="([^"]*)"/g;
+  let match;
+   while ((match = regex.exec(line)) !== null) {
+    const url = new URL(match[1], baseURL);
+    // rl.write(url.toString() + "\n");
+    urls += url.toString() + "\n";
+  }
 });
 
 rl.on('close', () => {
   // TODO some code
+  process.stdout.write(urls);
+  // dont need to exit process, and write on end, that seemed to be the issue
 });
